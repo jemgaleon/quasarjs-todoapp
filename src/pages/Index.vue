77daad814e2a59615f81254bee8e0fca9e0d7ff3
jemgaleon <jemgaleon@gmail.com>
2020-01-05 23:53:51 +0800

    Bug fixes and some improvements * improve UI * improve checking of what textbox is currently being edited * remove blur event on todo edit textbox * add success notifications on archived and unarchived * clear text in todo add textbox on blur * disable add and save button instead of notification on empty text * set todo edit textbox initial value instead of empty text on

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,15 +1,18 @@
 <template>
   <q-page padding>
-    <div class="row q-pa-sm">
+    <div class="row q-pa-sm q-gutter-md">
       <div class="col">
         <q-input dense
           v-model="addDescription" 
           label="Add todo item"
           ref="focusAdd"
+          @blur="clearAddTodo"
+          @focus="focusAddTodo"
           @keyup.enter="addTodo" />
       </div>
       <q-btn round color="primary" icon="add" size="md"
-        @click="addTodo" />
+        @click="addTodo"
+        :disabled="!addDescription.trim()" />
     </div>
     <div class="row q-pa-sm">
       <div class="col">
@@ -21,16 +24,16 @@
               :key=index>
               <q-item-section side top>
                 <q-checkbox 
-                  v-model="todo.completed" />
+                  :value="todo.completed"
+                  @click.native="toggle(index)" />
               </q-item-section>
 
               <q-item-section>
                 <q-item-label>
                   <q-input dense
                     v-model="updateDescription" 
-                    v-if="todo.editing"
+                    v-if="editIndex === index"
                     ref="focusEdit"
-                    @blur="cancelEdit(index, $event)"
                     @keyup.enter="saveTodo(index)" />
                   <span 
                     v-else 
@@ -39,29 +42,30 @@
                   </span>
                 </q-item-label>
                 <q-item-label caption
-                  v-if="!todo.editing">
+                  v-if="editIndex !== index">
                   Created on {{todo.dateCreated.toLocaleDateString()}}
                 </q-item-label>
               </q-item-section>
 
-              <q-item-section>
-                <div class="row">
+              <q-item-section side>
+                <div class="row q-gutter-sm">
                   <q-btn round color="primary" icon="save" size="md" 
-                    v-if="todo.editing"
-                    @click="saveTodo(index)"/>
+                    v-if="editIndex === index"
+                    @click="saveTodo(index)"
+                    :disable="!updateDescription.trim()"/>
                   <q-btn round color="primary" icon="edit" size="md" 
                     v-else 
-                    @click="editTodo(index)" 
+                    @click="editTodo(index)"
                     :disable="todo.completed ? true : false" />
                   <q-btn round color="negative" icon="not_interested" size="md" 
-                    v-if="todo.editing" 
+                    v-if="editIndex === index" 
                     @click="cancelEdit(index)"/>
                   <q-btn round color="negative" icon="delete" size="md" 
                     v-else 
                     @click="deleteTodo(index)"/>
                   <q-btn round color="warning" icon="archive" size="md" 
                     @click="archiveTodo(index)"
-                    :disable="todo.editing ? true : false"/>
+                    :disable="editIndex === index"/>
                 </div>
               </q-item-section>
             </q-item>
@@ -95,8 +99,8 @@
                 </q-item-label>
               </q-item-section>
 
-              <q-item-section>
-                <div class="row">
+              <q-item-section side>
+                <div class="row q-gutter-sm">
                   <q-btn round color="primary" icon="unarchive" size="md" 
                     @click="unarchiveTodo(index)"/>
                 </div>
@@ -116,67 +120,88 @@ export default {
     return {
       addDescription: "",
       updateDescription: "",
+      editIndex: -1,
       todos: [
-        { description: "Fix interface", completed: false, dateCreated: new Date("12/11/2019"), editing: false },
-        { description: "Fix focus add", completed: false, dateCreated: new Date("12/11/2019"), editing: false },
-        { description: "Backend API", completed: false, dateCreated: new Date("12/11/2019"), editing: false },
+        { description: "Fix interface", completed: false, dateCreated: new Date("12/11/2019") },
+        { description: "Fix focus add", completed: false, dateCreated: new Date("12/11/2019") },
+        { description: "Backend API", completed: false, dateCreated: new Date("12/11/2019") },
       ],
       archives: [
-        { description: "Add archive", completed: true, dateCreated: new Date("12/09/2019"), editing: false },
+        { description: "Add archive", completed: true, dateCreated: new Date("12/09/2019") },
       ],
     }
   },
   methods: {
     addTodo() {
-      if (this.addDescription === '') {
-        this.$q.notify({
-          message: "Enter description",
-          color: "warning",
-        });
+      let todo = {
+        description: this.addDescription,
+        completed: false,
+        dateCreated: new Date(),
       }
-      else {
-        let todo = {
-          description: this.addDescription,
-          completed: false,
-          dateCreated: new Date(),
-          editing: false,
-        }
 
-        this.todos.unshift(todo);
-        this.addDescription = "";
-        this.$q.notify({
-          message: "Successfully added",
-          color: "secondary",
-        });
-      }
+      this.todos.unshift(todo);
+      this.addDescription = "";
+      this.editIndex = -1;
+
+      this.$q.notify({
+        message: "Successfully added",
+        color: "secondary",
+      });
     },
     editTodo(index, event) {
-      this.todos[index].editing = true;
+      this.updateDescription = this.todos[index].description;
+      this.editIndex = index;
       this.focusOnActiveInput();
     },
     deleteTodo(index) {
+      this.editIndex = -1;
+
       this.$q.dialog({
         title: 'Confirm',
         message: "Are you sure you want delete?",
         cancel: true,
-        persistent: true
       }).onOk(() => {
         this.todos.splice(index, 1);
       });
     },
     saveTodo(index) {
-      if (this.updateDescription === '') {
-        this.$q.notify({
-          message: "Enter description",
-          color: "warning",
-        });
+      this.todos[index].description = this.updateDescription.trim();
+      this.updateDescription = "";
+      this.editIndex = -1;
 
-        this.focusOnActiveInput();
-      }
-      else {
-        this.todos[index].description = this.updateDescription;
-        this.todos[index].editing = false;
+      this.$q.notify({
+        message: "Successfully updated",
+        color: "secondary",
+      });
+    },
+    cancelEdit(index) {
+      if (this.editIndex > -1)
+      {
         this.updateDescription = "";
+        this.editIndex = -1;
+      }
+    },
+    archiveTodo(index) {
+      let archive = this.todos.splice(index, 1);
+
+      if (archive.length)
+      {
+        this.archives.unshift(archive[0]);
+        this.editIndex = -1;
+  
+        this.$q.notify({
+          message: "Successfully updated",
+          color: "secondary",
+        });
+      }
+    },
+    unarchiveTodo(index) {
+      let archive = this.archives.splice(index, 1);
+
+      if (archive.length)
+      {
+        this.todos.push(archive[0]);
+        this.editIndex = -1;
 
         this.$q.notify({
           message: "Successfully updated",
@@ -184,33 +209,23 @@ export default {
         });
       }
     },
-    cancelEdit(index, event) {
-      if (event && this.todos[index].editing) { // skip blur if cancel button click
-        this.todos[index].editing = false;
-        return false;
-      }
-
-      this.todos[index].editing = false;
-      this.updateDescription = "";
+    toggle(index) {
+      this.todos[index].completed = !this.todos[index].completed;
     },
-    archiveTodo(index) {
-      let archive = this.todos.splice(index, 1);
-
-      if (archive.length)
-        this.archives.push(archive[0]);
+    clearAddTodo() {
+      setTimeout(() => {
+        this.addDescription = "";
+      }, 300);
     },
-    unarchiveTodo(index) {
-      let archive = this.archives.splice(index, 1);
-
-      if (archive.length)
-        this.todos.push(archive[0]);
+    focusAddTodo() {
+      this.editIndex = -1;
     },
-    focusOnActiveInput() {
+    focusOnActiveInput(mode) {
       this.$nextTick(() => {
         let index = this.$refs.focusEdit.length - 1;
         this.$refs.focusEdit[index].$el.focus();
       });
-    },
+    }
   }
 }
 </script>
